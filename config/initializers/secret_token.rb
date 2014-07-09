d3ddf3cef5ff65f623694459da9e18cd1d451ae7
furfar <liujun2008116@126.com>
2014-07-09 12:32:44 +0800

    create rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foods::Application.config.secret_key_base = 'cf49031f2e62212812361248fe8b02a1e38e9ee50a84ee8a8fe849543195001bf73d43ddbe8ff655c1fd3f93d639b554989a9558e81fc094bfdc41ee67152405'
